ef302adbcd33ebf9bbcdc2ee61af23afeb0676e7
Jamie Cameron <jcameron@webmin.com>
2009-06-12 21:46:03 +0000

    Participate in Webmin search

--- cgi_args.pl
+++ cgi_args.pl
@@ -7,12 +7,11 @@ my ($cgi) = @_;
 &foreign_require("virtual-server", "virtual-server-lib.pl");
 my ($d) = grep { &virtual_server::can_edit_domain($_) &&
 		  $_->{$module_name} } &virtual_server::list_domains();
-return undef if (!$d);
 if ($cgi eq 'config.cgi') {
-	return 'dom='.&urlize($d->{'dom'});
+	return $d ? 'dom='.&urlize($d->{'dom'}) : 'none';
 	}
 elsif ($cgi eq 'view.cgi') {
-	return 'config='.&urlize($d->{'dom'});
+	return $d ? 'config='.&urlize($d->{'dom'}) : 'none';
 	}
 return undef;
 }
